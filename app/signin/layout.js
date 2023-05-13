d65b0f6795b02f1b435009b065e0cc8223ae8baf
julian-blaschke <julianblaschke@icloud.com>
2023-05-13 23:46:59 +0200

    update heading margin

--- app/signin/layout.js
+++ app/signin/layout.js
@@ -5,8 +5,8 @@ export const metadata = {
 
 export default function Layout({ children }) {
   return (
-    <div className="pt-24 md:pt-8 px-4 h-full flex-1 grid items-start md:items-center justify-items-center bg-gradient-to-br from-secondary to-accent">
-      <div className="px-2 py-10 flex flex-col md:justify-center w-full max-w-sm bg-base-200 bg-opacity-0 md:bg-opacity-70 rounded-box md:shadow-2xl">{children}</div>
+    <div className="-pt-40 md:pt-8 px-4 h-full flex-1 grid items-center justify-items-center md:bg-gradient-to-br from-secondary to-accent">
+      <div className="px-2 py-10 flex flex-col md:justify-center w-full max-w-sm md:bg-base-200 rounded-box md:shadow-2xl">{children}</div>
     </div>
   )
 }
--- app/signin/page.js
+++ app/signin/page.js
@@ -3,7 +3,7 @@ import Icon from "@components/icon"
 export default function Signin() {
   return (
     <div className="px-4 flex flex-col">
-      <div className="pb-8 divider text-center text-2xl font-medium">Sign in / up</div>
+      <div className="mb-20 md:mb-0 pb-8 divider text-center text-2xl font-medium">Sign in / up</div>
       <div className="my-8 flex flex-col gap-4">
         <button className="btn btn-outline">Sign in with Instagram</button>
         <p className="pt text-xs text-base-content text-opacity-50">Accounts on Tattiary are closely related to your linked instagram account. We reuse stuff like username, profile picture, etc. and always keep everything up to date. That way you can save yourself time & trouble. </p>
