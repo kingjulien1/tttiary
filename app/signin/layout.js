3c213476915ae46b44b6b302928e99f1cd17c517
julian-blaschke <julianblaschke@icloud.com>
2023-05-13 23:05:48 +0200

    refactor: update mobile design

--- app/signin/layout.js
+++ app/signin/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
 
 export default function Layout({ children }) {
   return (
-    <div className="pt-6 my-20 md:my-0 px-4 h-full flex-1 grid md:items-center justify-items-center md:bg-gradient-to-br from-secondary to-accent">
+    <div className="pt-6 px-4 h-full flex-1 grid items-center justify-items-center bg-gradient-to-br from-secondary to-accent">
       <div className="px-2 py-10 flex flex-col justify-center w-full max-w-sm bg-base-200 rounded-box md:shadow-2xl">{children}</div>
     </div>
   )
