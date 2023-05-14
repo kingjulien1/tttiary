990702aca93d306a676df328a18e0b395fa4488d
Julian Blaschke <julianblaschke@icloud.com>
2023-05-14 22:21:10 +0200

    feat: integrating provisional logo

--- app/signin/layout.js
+++ app/signin/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
 
 export default function Layout({ children }) {
   return (
-    <div className="-pt-40 md:pt-8 px-4 h-full flex-1 grid items-center justify-items-center md:bg-gradient-to-br from-secondary to-accent">
+    <div className="pt-10 md:pt-0 px-4 h-full flex-1 grid grid-cols-1 items-center justify-items-center md:bg-gradient-to-br from-secondary to-accent">
       <div className="px-2 py-10 flex flex-col md:justify-center w-full max-w-sm md:bg-base-200 rounded-box md:shadow-2xl">{children}</div>
     </div>
   )
--- app/signin/page.js
+++ app/signin/page.js
@@ -1,10 +1,13 @@
-import Icon from "@components/icon"
+import Image from "next/image"
 
 export default function Signin() {
   return (
     <div className="px-4 flex flex-col">
-      <div className="mb-20 md:mb-0 pb-8 divider text-center text-2xl font-medium">Sign in / up</div>
-      <div className="my-8 flex flex-col gap-4">
+      <div className="pb-4 flex flex-col items-center">
+        <Image height={50} width={50} src="/skull.png" alt="logo" />
+      </div>
+      <div className="divider text-center text-xl md:text-2xl font-medium">Join the Network</div>
+      <div className="py-10 pb-6 flex flex-col gap-4">
         <button className="btn btn-outline">Sign in with Instagram</button>
         <p className="pt text-xs text-base-content text-opacity-50">Accounts on Tattiary are closely related to your linked instagram account. We reuse stuff like username, profile picture, etc. and always keep everything up to date. That way you can save yourself time & trouble. </p>
       </div>
